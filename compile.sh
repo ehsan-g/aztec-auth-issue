405f6703832dc7d848eef5ef8a83c4cda6c29242
G <ehsan@say.company>
2025-11-07 16:17:33 +0330

    upgraded - unauthorized

--- compile.sh
+++ compile.sh
@@ -4,9 +4,9 @@ cd packages/token && RUST_BACKTRACE=1 aztec-nargo compile && aztec-postprocess-c
 wait
 cd ../../
 
-# cd packages/factory && RUST_BACKTRACE=1 aztec-nargo compile && aztec-postprocess-contract && rm -rf ./codegenCache.json  && aztec codegen target --outdir src/artifacts
-# wait
-# cd ../../
+cd packages/factory && RUST_BACKTRACE=1 aztec-nargo compile && aztec-postprocess-contract && rm -rf ./codegenCache.json  && aztec codegen target --outdir src/artifacts
+wait
+cd ../../
 
-# cd packages/router && RUST_BACKTRACE=1 aztec-nargo compile && aztec-postprocess-contract && rm -rf ./codegenCache.json  && aztec codegen target --outdir src/artifacts
-# wait
+cd packages/router && RUST_BACKTRACE=1 aztec-nargo compile && aztec-postprocess-contract && rm -rf ./codegenCache.json  && aztec codegen target --outdir src/artifacts
+wait
